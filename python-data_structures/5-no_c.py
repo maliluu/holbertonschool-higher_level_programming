b6e755737c376b79ca385615f154f2aad120303a
mal <159127791+maliluu@users.noreply.github.com>
2024-05-26 20:09:43 +0300

    Update 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,1 @@
-#!/usr/bin/python3def no_c(my_string):    new_string = ""    for i in range(len(my_string)):        element = my_string[i]    if element.lower() not in ('c'):        new_string += element    return new_string
+#!/usr/bin/python3def no_c(my_string):    new_string = ""    for i in range(len(my_string)):        element = my_string[i]    if element.lower() not in ('c'):        new_string += element        return new_string
