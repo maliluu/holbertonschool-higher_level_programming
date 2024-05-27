cf093b0154f47fd90379cb119f2bf038fefcef1d
mal <159127791+maliluu@users.noreply.github.com>
2024-05-27 12:38:36 +0300

    Update 9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,1 @@
+#!/usr/bin/python3def max_integer(my_list=[]):    if len(my_list) == 0:        return None    max_var = my_list[0];    for i in range(len(my_list)):        if max_var < my_list[i]:            max_var = my_list[i]    return max_var
