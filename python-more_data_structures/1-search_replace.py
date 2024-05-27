fffd8965a85f6077fc40bfeed8db49c66116fd31
mal <159127791+maliluu@users.noreply.github.com>
2024-05-27 15:49:25 +0300

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,1 @@
-#!/usr/bin/python3def search_replace(my_list, search, replace):    new_list = my_list.copy()    for i in range(len(my_list)):        if my_list[i] == search:            new_list[i] = replace            return new_list
+#!/usr/bin/python3def search_replace(my_list, search, replace):    new_list = my_list.copy()    for i in range(len(my_list)):        if my_list[i] == search:            new_list[i] = replace            return new_list        return new_list
