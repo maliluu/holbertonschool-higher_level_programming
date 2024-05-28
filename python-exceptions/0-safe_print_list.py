0219432626192fbef80eb36f35e28b9aae55ebee
mal <159127791+maliluu@users.noreply.github.com>
2024-05-28 22:17:30 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    counter = 0
 
+def safe_print_list(my_list=[], x=0):
+    counter=0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
+            print("{}".format(my_list[i]),end='')
         except:
             break
         else:
