99a5328b5e31d6c52b726d5c8175ef8859c32006
mal <159127791+maliluu@users.noreply.github.com>
2024-05-28 22:17:59 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     counter=0
     for i in range(x):
@@ -9,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
             break
         else:
             counter += 1
-
     print()
     return counter
