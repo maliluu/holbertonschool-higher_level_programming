683134a66b6dc729536f4c104b0c03bd38f62869
mal <159127791+maliluu@users.noreply.github.com>
2024-05-28 11:40:51 +0300

    Update 9-multiply_by_2.py

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,1 @@
+#!/usr/bin/python3def multiply_by_2(a_dictionary):    dictionary = a_dictionary.copy()    keys = list(dictionary.keys())    for i in keys:        dictionary[i] *= 2    return (dictionary)
