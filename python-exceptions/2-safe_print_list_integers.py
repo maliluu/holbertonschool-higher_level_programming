a6911584ba99d34bb49f9bb3a7eea3115d174e0c
mal <159127791+maliluu@users.noreply.github.com>
2024-05-28 17:50:02 +0300

    Update 2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,1 @@
+#!/usr/bin/python3def safe_print_list_integers(my_list=[], x=0):    count = 0    for i in range(x):        try:            print("{:d}".format(my_list[i]), end="")        except TypeError:            pass # Break = breaks the loop while Pass is like a pause        except ValueError:            pass        else:            count += 1    print()    return (count)
