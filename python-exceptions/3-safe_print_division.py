c9808a032247a49d070aed36d4e8317de15fda56
mal <159127791+maliluu@users.noreply.github.com>
2024-06-09 17:21:07 +0300

    Update 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,1 @@
-#!/usr/bin/python3def safe_print_division(a, b):    try:        div_result = a / b    except:        div_result = None    finally:        print("Inside result: {}".format(div_result))        return div_result
+#!/usr/bin/python3def safe_print_division(a, b):    """Returns the division of a by b."""    try:        div = a / b    except (TypeError, ZeroDivisionError):        div = None    finally:        print("Inside result: {}".format(div))    return (div)
